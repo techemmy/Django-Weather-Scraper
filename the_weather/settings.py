2de3aae87dc0ce48c47c370850d735e2d11e5aad
Emmanuel <emmanueltopea@gmail.com>
2019-09-24 23:06:30 +0100

    Weathie Forecast App

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY =  os.environ.get('BLOG_KEY')
 
-#'_%8&aaez4#5%6)$$#rxdbyv5)(he7(2&@_d77_00tehjkrj$n2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
